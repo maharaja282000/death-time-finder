b21e75486be416048d6fd9771753269dd5da33d7
maharaja282000 <68776313+maharaja282000@users.noreply.github.com>
2020-07-25 18:13:43 +0530

    Add files via upload

---
+++ murder case (death time).py
@@ -0,0 +1,24 @@
+print("DEATH*** TIME*** PREDICTOR")
+c=float(input("enter the Ti-'temperature shown by thermometer......'"))
+d=float(input("enter the (t)-'temp after x hours......'"))
+a=float(input("enter the T(t)-'measure after x hours......' "))
+b=float(input("enter the Ts-'surrounding temperature......'"))
+e=((a-b)/(c-b))
+print("the value of e pow -kd.....",e)
+import math
+f=math.log(e)
+print("the value till -kxd is",f)
+g=f/d
+print("the value of k is",g)
+a=37
+h=((a-b)/(c-b))
+j=math.log(h)
+print("the value of gxt is......",j)
+k=j/g
+print("the time of death is.............",k)
+import math
+print(math.ceil(k),"the appx death time is")
+      
+
+
+
